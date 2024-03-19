1484c5621d6cfb8571799b0c68086489e7e2bd4b
WingKill <zpswlsldi@gmail.com>
2024-03-19 17:45:27 +0900

    footer 꾸미기, 글쓰기 버튼을 modal 창으로 만들어보기 2

---
+++ jsp_ss_game/src/edu/sejong/game/controller/GugudanController.java
@@ -0,0 +1,67 @@
+package edu.sejong.game.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class lottoController
+ */
+@WebServlet("/gugudan")
+public class GugudanController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GugudanController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request,response);
+	}
+
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		
+		String viewPage = null;
+		
+		String uri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String comm = uri.substring(contextPath.length());
+		
+		System.out.println("uri : " + uri);
+		System.out.println("contextPath : " + contextPath);
+		System.out.println("comm : " + comm);
+		
+		if(comm.equals("/gugudan")) {			
+			viewPage = "/gugudan.html";
+		}
+		
+		if(viewPage != null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
